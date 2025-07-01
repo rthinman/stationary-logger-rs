a9f7643f1f8215cd3c240a17a1d2903372d5a239
Rod Hinman <rod@auroraresearch.com>
2025-06-30 17:39:12 -1000

    RTC is now a RELT timestamper
    Only implements RELT and RTCW in seconds, no ISO 8601 durations format yet.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 mod fmt;
 
+use core::f32::consts;
+
 #[cfg(not(feature = "defmt"))]
 use panic_halt as _;
 use crate::fmt::unwrap;
@@ -57,6 +59,148 @@ where
     }
 }
 
+const RTC_BACKUP_KEY_INDEX: usize = 0; // Index to RTC backup register where key is stored
+const RTC_BACKUP_RTCW_INDEX: usize = 1; // Index to RTC backup register where RTCW is stored
+const RTC_BACKUP_KEY_VALUE: u32 = 0xA53C4B69; // Value stored at RTC_BACKUP_KEY_INDEX if RTCW value is good
+const EMBASSY_DATETIME_OFFSET: u16 = 2000; // Offset for the year in DateTime, since embassy-stm32 uses 2000-2099, but the RTC uses 0-99.
+
+struct Timestamp {
+    rtc: Rtc, // <'static, embassy_stm32::rtc::RtcConfig>
+    rtcw: u32,
+}
+
+impl Timestamp {
+    /// Create a new Timestamp instance if the RTC is already running.
+    pub fn from_running(mut rtc: Rtc) -> Self {
+        let rtcw = rtc.read_backup_register(RTC_BACKUP_RTCW_INDEX)
+            .unwrap_or(0); // Read the RTCW value from the backup register, or 0 if not set.
+        Self { rtc, rtcw }
+    }
+    /// Create a new Timestamp instance with a specific RTCW value.
+    /// This is typically used when the RTC is starting from a power outage.
+    pub fn from_rtcw(mut rtc: Rtc, rtcw: u32) -> Self {
+        // Store the RTCW value in the backup register.
+        rtc.write_backup_register(RTC_BACKUP_RTCW_INDEX, rtcw);
+        // Write the key to the backup register to indicate that the RTCW value is valid.
+        rtc.write_backup_register(RTC_BACKUP_KEY_INDEX, RTC_BACKUP_KEY_VALUE);
+        // Convert to datetime and set RTC.
+        let dt = Timestamp::seconds_to_datetime(rtcw);
+        rtc.set_datetime(dt).expect("Failed to set datetime");
+        // Return the Timestamp instance.
+        Self { rtc, rtcw }
+    }
+
+    /// Get RELT, the uptime in seconds since first boot.
+    pub fn get_uptime_seconds(&self) -> u32 {
+        // Get the current time in seconds since the epoch.
+        let now = self.rtc.now().unwrap();
+        // Convert to seconds since the epoch (0, 3, 1).
+        Timestamp::datetime_to_seconds(now).expect("Failed to convert datetime to seconds")
+    }
+
+    /// Get RTCWake, the value of RELT at the last "brownout" event.
+    pub fn get_rtcw(&self) -> u32 {
+        self.rtcw
+    }
+
+    // Static methods for Timestamp
+
+    /// Check if the RTC is running.
+    pub fn is_running(rtc: &Rtc) -> bool {
+        // Check if the RTC is running by reading the backup register.
+        rtc.read_backup_register(RTC_BACKUP_KEY_INDEX).unwrap_or(0) == RTC_BACKUP_KEY_VALUE
+    }
+
+    /// Convert seconds to days, hours, minutes, and remaining seconds.
+    pub fn seconds_to_dhms(seconds: u32) -> (u32, u32, u32, u32) {
+        // Size constraints:
+        // days can be up to 49_710, or could fit in a u16, should we want.
+        // seconds_of_day still needs a u32.
+        // hours, minutes, remaining_seconds could all fit in u8.
+        let days = seconds / 86400; // Because there are 86400 seconds in a day.
+        let seconds_of_day = seconds - days * 86400; // = seconds % 86400
+        let hours = seconds_of_day / 3600;
+        let remaining_seconds = hours * 3600;
+        let minutes = remaining_seconds / 60;
+        let remaining_seconds = remaining_seconds - minutes * 60;
+
+        (days, hours, minutes, remaining_seconds)
+    }
+
+    /// Convert seconds since the epoch (0, 3, 1) to a DateTime.
+    pub fn seconds_to_datetime(seconds: u32) -> DateTime {
+        // Get the total number of days represented, plus the time of day.
+        // A u32 can hold up to 49,710 days, which is about 136 years.
+        let (days, hour, minute, second) = Timestamp::seconds_to_dhms(seconds);
+
+        // Calculate the Julian date from the number of days, since computational epoch
+        // March 1, 0000.
+        // Based on the algorithm from below, but not yet using the Euclidean 
+        // affine function optimizations:
+        // Neri C, Schneider L. "Euclidean affine functions and their application 
+        // to calendar algorithms". Softw Pract Exper. 2022;1-34. doi: 10.1002/spe.3172.
+        // https://onlinelibrary.wiley.com/doi/full/10.1002/spe.3172
+        // References to variable names are to the paper above, Section 5, which uses uppercase.
+        let n_1 = 4 * days + 3;  // N1
+        let year_computational = n_1 / 1461;  // Y
+        let n_y = n_1 % 1461 / 4; // N_Y
+        let n_2 = 5 * n_y + 461;  // N_2
+        let m = n_2 / 153; // M
+        let day_julian: u8 = (n_2 % 153 / 5 + 1) as u8; // D_J (skipped computing D)
+        // J = 1{M>=13}
+        let j = if m >= 13 {
+            1
+        } else {
+            0
+        };
+
+        let month_julian: u8 = (m - 12 * j) as u8; // M_J
+        // Embassy's DateTime uses a year offset of 2000, so we need to add that.
+        let year_julian: u16 = (year_computational + j) as u16 + EMBASSY_DATETIME_OFFSET; // Y_J + 2000
+
+        // We do not use day of week, so the choice is arbitrary.
+        DateTime::from(year_julian, month_julian, day_julian, embassy_stm32::rtc::DayOfWeek::Monday, hour as u8, minute as u8, second as u8).expect("Invalid date")
+    }
+
+    /// Convert a DateTime to seconds since the epoch (0, 3, 1) Julian date.
+    pub fn datetime_to_seconds(datetime: DateTime) -> Option<u32> {
+        // The STM32 RTC only holds year values 0-99.
+        // Embassy's DateTime assumes the year is 2000-2099, so get last two digits.
+
+        let year: u32 = (datetime.year() % 100).into(); 
+        let month: u32 = datetime.month().into();
+        if year == 0 && month < 3 {
+            // The computational calendar does not support this combination with
+            // unsigned values, and we should always initialize the RTC to later than this.
+            return None;
+        }
+        let day: u32 = datetime.day().into();
+        let hour: u32 = datetime.hour().into();
+        let minute: u32 = datetime.minute().into();
+        let second: u32 = datetime.second().into();
+
+        let j: u32 = if month <= 2 {
+            // This is because the computational calendar starts on March 1st.
+            1
+        } else {
+            0
+        };
+        let y = year - j;
+        let m = month + 12 * j; // Month is 1-12, but we need to adjust for the computational calendar.
+        let d = day - 1; // Day is 1-31.
+
+        let y0 = 1461 * y / 4;
+        let m0 = (153 * m - 457) / 5;
+
+        let days_since_epoch =  y0 + m0 + d;
+
+        // Calculate the total seconds since the epoch.
+        Some(days_since_epoch * 86400 + hour * 3600 + minute * 60 + second)
+    }
+
+}
+
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
 
@@ -106,13 +250,36 @@ async fn main(spawner: Spawner) {
     let mut led = Output::new(p.PB0, Level::High, Speed::Low);
     let mut btn = ExtiInput::new(p.PB5, p.EXTI5, Pull::Up);
 
-    // RTC, from embassy example, but not using the chrono crate.
-    let now: DateTime = DateTime::from(
-        2025, 6, 19, embassy_stm32::rtc::DayOfWeek::Thursday, 9, 42, 0).expect("invalid date");
+    // Temporary test code to check the conversion from seconds to DateTime.
+    let dt = Timestamp::seconds_to_datetime(0);
+    info!("should be march 1 2000: {}-{:02}-{:02} {:02}:{:02}:{:02}", 
+        dt.year(), dt.month(), dt.day(), dt.hour(), dt.minute(), dt.second());
+    let dt = Timestamp::seconds_to_datetime(86400);
+    info!("should be march 2 2000: {}-{:02}-{:02} {:02}:{:02}:{:02}", 
+        dt.year(), dt.month(), dt.day(), dt.hour(), dt.minute(), dt.second());
 
+    let reconverted_seconds = Timestamp::datetime_to_seconds(dt).expect("Failed to convert DateTime to seconds");
+    info!("Reconverted seconds: {}", reconverted_seconds);
+
+    let dt2: DateTime = DateTime::from(2000, 8, 15, embassy_stm32::rtc::DayOfWeek::Monday, 0, 0, 0)
+        .expect("Failed to create DateTime");
+    let seconds = Timestamp::datetime_to_seconds(dt2).expect("Failed to convert DateTime to seconds");
+    // Should be 14,428,800.
+    info!("DateTime 2000-08-15 00:00:00 converted to seconds: {}", seconds);
+    
+        // RTC initialization
     let mut rtc = Rtc::new(p.RTC, RtcConfig::default());
     rtc.set_daylight_savings(false);
-    rtc.set_datetime(now).expect("datetime not set");
+    let timestamp = if Timestamp::is_running(&rtc) {
+        info!("RTC is running, using existing RTCW value...");
+        Timestamp::from_running(rtc)
+    } else {
+        // RTC was not running, so we need to initialize it.
+        info!("RTC not running, initializing...");
+        let rtcw = 0_u32; // TODO: Get the RTCW value from non-volatile storage or set to 0.
+        Timestamp::from_rtcw(rtc, rtcw)
+    };
+
 
     // I2C and temp sensor initialization.
     bind_interrupts!(struct Irqs {
@@ -143,14 +310,14 @@ async fn main(spawner: Spawner) {
         match CHANNEL.receive().await {
             Events::Button(ButtonEvent::Pressed) => {
                 info!("Button pressed event received");
-                let then = rtc.now().unwrap();
-                info!("time: {:?}:{:?}", then.minute(), then.second());
+                // let then = rtc.now().unwrap();
+                // info!("time: {:?}:{:?}", then.minute(), then.second());
             }
             Events::Button(ButtonEvent::Released) => {
                 info!("Button released event received");
             }
             Events::TempReading(temperature) => {
-                info!("Temperature: {} °C", temperature);
+                info!("Time: {}, Temperature: {} °C", timestamp.get_uptime_seconds(), temperature);
             }
         }
 
@@ -199,5 +366,29 @@ async fn get_temperature(
             Err(_) => warn!("Failed to read from temperature sensor"),
         }
         ticker.next().await;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use embassy_stm32::rtc::DateTime;
+
+    #[test]
+    fn test_seconds_to_datetime() {
+        // Example: 0 seconds since epoch should be March 1, 2000, 00:00:00
+        let dt = seconds_to_datetime(0);
+        assert_eq!(dt.year(), 2000);
+        assert_eq!(dt.month(), 3);
+        assert_eq!(dt.day(), 1);
+        assert_eq!(dt.hour(), 0);
+        assert_eq!(dt.minute(), 0);
+        assert_eq!(dt.second(), 0);
+
+        // Example: 86400 seconds (1 day) since epoch should be March 2, 2000
+        let dt = seconds_to_datetime(86400);
+        assert_eq!(dt.year(), 2000);
+        assert_eq!(dt.month(), 3);
+        assert_eq!(dt.day(), 2);
     }
 }
